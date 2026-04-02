9fc7feea446253c3cf04f620cc6f14021f12d04c
Noaht8 <noahtsegay8@gmail.com>
2026-04-02 11:01:42 +0300

    FPS

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
+
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +21,10 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        c = clock.tick(60)
+        dt = c / 1000
+        
+
 
 
 
